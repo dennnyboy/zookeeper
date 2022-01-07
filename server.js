64a7fa06803fe5b86828a3b430aff646650b2874
dennnyboy <Dennis11@live.com>
2022-01-06 19:45:31 -0500

    adding commit

--- server.js
+++ server.js
@@ -42,6 +42,11 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0]
+  return result
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals
     if (req.query) {
@@ -50,7 +55,16 @@ app.get('/api/animals', (req, res) => {
     res.json(results)
   });
 
-app.listen(3001, () => {
-    console.log('API server now set to 3001')
+app.get('/api/animals/:id', (req,res) => {
+  const result = findById(req.params.id, animals)
+  if(result){
+  res.json(result)
+  } else {
+    res.sendStatus(404)
+  }
+})
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`)
 
 })
